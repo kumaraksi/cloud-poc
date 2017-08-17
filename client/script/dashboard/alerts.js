257f85b504c8d549326290f27ec5a5381b2719ba
Surendra Borana <sborana@cisco.com>
2017-08-17 18:43:42 +0530

    Alerts Tab Completed

--- client/script/dashboard/alerts.js
+++ client/script/dashboard/alerts.js
@@ -1,14 +1,62 @@
-function updateAlerts(){
+
+window.alertTimeFilterDD = $("#alertTimeFilter").dropdown({ "autoinit" : ".select" , "optionClass": "withripple"});
+window.alertSeverityFilterDD = $("#alertSeverityFilter").dropdown({ "autoinit" : ".select" , "optionClass": "withripple"});
+
+
+
+window.alertFilter = {};
+window.alertFilter.history = 'allHealth';
+window.alertFilter.severity = null;
+var ts = Math.round(new Date().getTime());
+var tsYesterday = ts - (24 * 3600 * 1000);
+window.alertFilter.time = tsYesterday;
+
+
+
+$('#alertTimeFilter').on('change', function(event, value){
 	
-	console.info('Alerts Updated');
+	var val = $(event.target).val();
+	if(val == '24'){
+		var ts = Math.round(new Date().getTime() );
+		var tsYesterday = ts - (24 * 3600 * 1000);
+		window.alertFilter.time = tsYesterday;
+	}else{
+		window.alertFilter.time = null;
+	}
 	
-	/*if(typeof window.eventsTable == 'undefined'){
-		window.eventsTable = $('#dataTables-example').DataTable({
-            responsive: true
-        });
-	}*/
+	updateAlerts();
+	
+});	
+
+$('#alertSeverityFilter').on('change', function(event, value){
+	var val = $(event.target).val();
+	console.info(val);
+	if(val == 0){
+		window.alertFilter.severity = null;
+	}else{
+		window.alertFilter.severity  = [val];
+	}
+	
+	updateAlerts();
+});
+/*
+$('#alertCurrentStatusFilter').on('change', function(event, value){
+	var val = $(event.target).val();
+	window.alertFilter.history = val;
+	
+	updateAlerts();
+});*/
+
+$('#alertSearchFilter').on('keyup', function(event){
+	var val = $(event.target).val();
+	window.alertsTable1.search(val).draw();
+});
+
+
+
+function getRecentCriticalAlerts(){
 	var url = utils.getBaseUrl() + 'alert/getAlerts';
-    var filterData = {
+	var filterData = {
 		"filter": {
 			"pageInfo": {
 				"start": 0,
@@ -19,11 +67,72 @@ function updateAlerts(){
 				"name": "alertTime",
 				"sortOrder": "DESC"
 			},
-			//"byLocationUids": [window.currentSelectedLocationUID],
-			"bySeverity":["CRITICAL", "INFO"]
+			"byAfterAlertTimeUTC" : window.alertFilter.time,
+			"bySeverity": ["CRITICAL"]
 			
 		}
 	};
+	var onSuccess = function(data, textStatus, jqXHR) {
+        data = JSON.parse(data);
+        console.info(data);
+		var alertList = data.data.items;
+		var totalAlerts = alertList.length;
+		$('#alertCountBadgeCount').text(totalAlerts);
+		if(totalAlerts > 0){
+			$('#alertCountBadge').addClass('critical');
+		}else{
+			$('#alertCountBadge').removeClass('critical');
+		}
+	}
+	
+	onError = function(jqXHR, textStatus, errorThrown) {
+		 console.warn(errorThrown);
+	};
+	
+	 utils.sendPost(url, filterData, onSuccess, onError);
+}
+getRecentCriticalAlerts();
+var updateAlertBadge = setInterval(getRecentCriticalAlerts, 10000);
+$('#alertCountBadgeCount').on('click', function(){
+	$('.nav-tabs a[href="#alerts"]').tab('show');
+});
+
+
+function getAlertFilters(){
+	var filterData = {
+		"filter": {
+			"pageInfo": {
+				"start": 0,
+				"limit": 100,
+				"skipTotalRowCount": true
+			},
+			"sortCriteria": {
+				"name": "alertTime",
+				"sortOrder": "DESC"
+			}
+		}
+	};
+	if(window.alertFilter.severity){
+		filterData.filter.bySeverity = window.alertFilter.severity;
+	}
+	filterData.filter[window.alertFilter.history] = true;
+	if(window.alertFilter.time){
+		filterData.filter.byAfterAlertTimeUTC = window.alertFilter.time;
+	}
+	return filterData;
+}
+
+function updateAlerts(){
+	
+	console.info('Alerts Updated');
+	
+	/*if(typeof window.eventsTable == 'undefined'){
+		window.eventsTable = $('#dataTables-example').DataTable({
+            responsive: true
+        });
+	}*/
+	var url = utils.getBaseUrl() + 'alert/getAlerts';
+    var filterData = getAlertFilters();
 	
 	var onSuccess = function(data, textStatus, jqXHR) {
         data = JSON.parse(data);
@@ -32,8 +141,10 @@ function updateAlerts(){
         
 		var alertDTData = [];
 		
+		
 		var totalAlerts = alertList.length;
-		$('#alertCountBadge').text(totalAlerts);
+		
+		$('#alertTableCountBadge').text(totalAlerts);
 		var alert, alertTime,alertDT;
 		for(var i=0; i < totalAlerts; i++){
 			alert = alertList[i];
@@ -45,6 +156,8 @@ function updateAlerts(){
 			window.alertsTable1 = $('#alertsTable').DataTable( {
 				data: alertDTData,
 				autoWidth : false,
+				"dom": '<"top"i>rt<"bottom"lp><"clear">',
+				"processing": true,
 				columns: [
 					{ 
 						title: "Severity", 
@@ -90,6 +203,9 @@ function updateAlerts(){
 								case "INFO":
 									return '<span class="label label-info">INFO</span>';
 									break;
+								case "WARNING":
+									return '<span class="label label-warning">WARNING</span>';
+									break;
 								default :
 									return '<span class="label label-primary">'+data+'</span>';
 							}
@@ -124,9 +240,11 @@ function updateAlerts(){
 				//scrollCollapse: true,
 				paging:         false
 			} );
+			
+			
+			
 		}else{
 			window.alertsTable1.clear().rows.add(alertDTData).draw();
-			
 		}
 		
 		$('#alertsTable tbody td').each(function(index){
